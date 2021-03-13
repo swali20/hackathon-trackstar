22a076699bc86080bc23c9aa9effd726ffcdfece
Siray Wali <siray.yaya@gmail.com>
2021-03-13 00:31:58 -0600

    chore(git): fixes store/index.js

--- client/store/index.js
+++ client/store/index.js
@@ -2,17 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-<<<<<<< HEAD
 import user from "./user";
 
 const reducer = combineReducers({
   user,
-=======
-import currentUser from "./user";
-
-const reducer = combineReducers({
-  currentUser,
->>>>>>> 705721ecbe8749b352254f1dacf4dabbe8f374c2
 });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
